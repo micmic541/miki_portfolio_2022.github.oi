f3d8d1c038bddb7d0e021db840bed8dc7e4ec5a9
micmic541 <mk0039unicorn@gmail.com>
2022-05-05 00:01:16 +0900

    Fixing JS for Title Animation
    毎回スクロールするたびに実行されていたアニメーションを一度に限定

--- scripts/main.js
+++ scripts/main.js
@@ -10,9 +10,9 @@ document.addEventListener('DOMContentLoaded',() => {
             const getElDistance = $targetTtls[i].getBoundingClientRect().top + $targetTtls[i].clientHeight * .5;
             if (innerHeight > getElDistance) {
                     ttlAnimation();
-            }
+            } 
         }
-    });
+    },{once: true});
 
     // ttl animation
     const ttlAnimation = () => {
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded',() => {
             for (let j = 0; j < ttlTxts.split('').length; j++) {
                 const txtSplit = ttlTxts.split('');
                 if (txtSplit[j] === '') {
-                    txtsArry.push(' ');
+                    txtsArry.push('  ');
                 } else {
                     txtsArry.push('<span><span style="animation-delay: ' + (j*.2) +'s;">' + txtSplit[j] + '</span></span>')
                 }
