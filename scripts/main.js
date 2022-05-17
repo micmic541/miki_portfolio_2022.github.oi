5bb9ac47693d1d72e40fd6ef93dbb5b8a4b180a1
micmic541 <mk0039unicorn@gmail.com>
2022-05-17 20:39:34 +0900

    Fixing title animation
    タイトルアニメーションの不具合を直しました。

--- scripts/main.js
+++ scripts/main.js
@@ -5,45 +5,46 @@ document.addEventListener('DOMContentLoaded',() => {
      $slideInFirst = $doc.querySelector('#js-scroll-trigger_first'),
      $slideIn = $doc.querySelectorAll('.js-scroll-trigger');
     
-     // --ttl animation--
-     const ttlAnimation = (els) => {
-         for (let i = 0; i < els.length; i++) {
-             const ttlEls = els[i],
-                     ttlTxts = ttlEls.textContent.trim(),
-                     txtsArry = [];
- 
-             ttlEls.textContent = '';
- 
-             for (let j = 0; j < ttlTxts.split('').length; j++) {
-                 const txtSplit = ttlTxts.split('');
-                 if (txtSplit[j] === ' ') {
-                     txtsArry.push('  ');
-                 } else {
-                     txtsArry.push('<span style="animation-delay: ' + (j*.2) +'s;">' + txtSplit[j] + '</span>')
-                 }
-             }
-             
-             for (let k = 0; k < txtsArry.length; k++) {
-                 ttlEls.innerHTML += txtsArry[k];
-             }
-         }
-     }
-     // animation trigger for title
-     const triggerTtl = (els) => {
-         for (let i = 0; i < els.length; i++) {
-             const rect = els[i].getBoundingClientRect().top;
-             const scroll = window.pageYOffset;
-             const offset = rect + scroll;
-             const windowHeight = window.innerHeight;
-             if (scroll > offset - windowHeight + 200) {
-                 return ttlAnimation(els);
-             }
-         }
-     }
- 
+    
+    // --ttl animation--
+    const options = {
+        threshold: 0
+    };
 
-    $doc.addEventListener('scroll',triggerTtl($targetTtls));
+    const observer = new IntersectionObserver(ttlAnimation,options);
 
+    $targetTtls.forEach(ttl => {
+        observer.observe(ttl);
+    });
+
+    function ttlAnimation(els){
+        els.forEach(el => {
+            if (el.isIntersecting) {
+                ttlTrigger(el.target);
+            }
+        });
+    }
+
+    function ttlTrigger(txt) {
+        let ttlEls = txt,
+            ttlTxts = ttlEls.textContent,
+            txtsArry = [];
+        
+        ttlEls.textContent = '';
+    
+        for (let j = 0; j < ttlTxts.split('').length; j++) {
+            const txtSplit = ttlTxts.split('');
+            if (txtSplit[j] === ' ') {
+                txtsArry.push('  ');
+            } else {
+                txtsArry.push('<span style="animation-delay: ' + (j*.2) +'s;">' + txtSplit[j] + '</span>')
+            }
+        }
+        
+        for (let k = 0; k < txtsArry.length; k++) {
+            ttlEls.innerHTML += txtsArry[k];
+        }
+    }
      
     // --animation for first contents--
     const animationFirst = () => {
@@ -71,47 +72,5 @@ document.addEventListener('DOMContentLoaded',() => {
         scrollIn($slideIn);
     }
 
-
-    // ---CODE THAT DIDNT WORK---animation trigger for title
-    //  const triggerTtl = (els) => {
-    //      for (let i = 0; i < els.length; i++) {
-    //          const rect = els[i].getBoundingClientRect().top;
-    //          const scroll = window.pageYOffset;
-    //          const offset = rect + scroll;
-    //          const windowHeight = window.innerHeight;
-    //          let $targetTtls = [];
-    //         if (scroll > offset - windowHeight + 200) {
-    //             $targetTtls = $doc.querySelectorAll('.js-ttl_01');
-    //             return ttlAnimation($targetTtls);
-    //         }
-    //         if (scroll > offset - windowHeight + 600) {
-    //             $targetTtls = $doc.querySelectorAll('.js-ttl_02');
-    //             return ttlAnimation($targetTtls);
-    //         }
-    //     }
-    // }
-
-    // // --ttl animation--
-    // const ttlAnimation = (els) => {
-    //     for (let i = 0; i < els.length; i++) {
-    //         const ttlEls = els,
-    //                 ttlTxts = ttlEls.textContent.trim(),
-    //                 txtsArry = [];
-
-    //         ttlEls.textContent = '';
-
-    //         for (let j = 0; j < ttlTxts.split('').length; j++) {
-    //             const txtSplit = ttlTxts.split('');
-    //             if (txtSplit[j] === ' ') {
-    //                 txtsArry.push('  ');
-    //             } else {
-    //                 txtsArry.push('<span style="animation-delay: ' + (j*.2) +'s;">' + txtSplit[j] + '</span>')
-    //             }
-    //         }
-    //     }
-    // }
-    
-    // window.addEventListener('scroll',triggerTtl($targetTtl));
-    
 
 });
