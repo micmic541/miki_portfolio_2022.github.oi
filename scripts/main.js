8a85d59c154f495761b40d99b623241ba2f76e13
micmic541 <mk0039unicorn@gmail.com>
2022-10-04 18:58:06 +0900

    Swiper
    I added Swiper for project section.

--- scripts/main.js
+++ scripts/main.js
@@ -74,4 +74,27 @@ document.addEventListener('DOMContentLoaded',() => {
         scrollIn($slideIn);
     }
 
+    // --Swiper
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+
 });
