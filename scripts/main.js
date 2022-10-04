0099fd49484b8408902ecd4613672b3cd4e9f388
micmic541 <mk0039unicorn@gmail.com>
2022-10-04 20:14:56 +0900

    Photos
    I added photos for project sections

--- scripts/main.js
+++ scripts/main.js
@@ -77,8 +77,9 @@ document.addEventListener('DOMContentLoaded',() => {
     // --Swiper
     const swiper = new Swiper('.swiper', {
         // Optional parameters
-        direction: 'vertical',
+        direction: 'horizontal',
         loop: true,
+        slidesPerView: 1,
       
         // If we need pagination
         pagination: {
@@ -89,12 +90,7 @@ document.addEventListener('DOMContentLoaded',() => {
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
+        }
       });
 
 });
